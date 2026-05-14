36966b8af36645857e6457672a32f02115c177e3
Jaiguru Prabu <jaiguruprabu@Jaigurus-MacBook-Pro.local>
2026-05-15 03:13:19 +0530

    Fixed dashboard layout import

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useCallback } from "react";
 
 import axios from "axios";
 
-import Layout from "../layouts/Layout";
+import DashboardLayout from "../layouts/DashboardLayout";
 
 import {
   Warehouse,
