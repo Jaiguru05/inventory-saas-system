a0657ffefa18a5a32039223f0510788e1ef84ce4
Jaiguru Prabu <jaiguruprabu@Jaigurus-MacBook-Pro.local>
2026-05-15 02:29:35 +0530

    Fixed URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -22,7 +22,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://inventory-saas-system.onrender.com",
+  baseURL: "https://inventory-saas-system.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
